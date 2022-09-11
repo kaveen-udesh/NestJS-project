3c724e152fa94d0d0dde7df0f2a97fa25eeb806f
kaveen-udesh <kaveenudesh@gmail.com>
2022-09-11 18:24:16 +0530

    Create controller

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,12 @@
+import {
+  Controller,
+  Get,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get('flavors')
+    findAll() {
+        return 'This action returns all coffees';
+    }
+}
