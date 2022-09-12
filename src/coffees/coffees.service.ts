d7712ff830af255e243541b64e6d4e52608611d9
kaveen-udesh <kaveenudesh@gmail.com>
2022-09-13 01:30:06 +0530

    Create rest api app

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,49 @@
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['Chocolate', 'Vanila'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    throw 'A random error';
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    return this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, createCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      //update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
